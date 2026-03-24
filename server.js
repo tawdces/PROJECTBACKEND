4d905ac9cf41274b3dfdcaa5d72e7b7be7d72ef7
unknown <jamecharun2007@gmail.com>
2026-03-24 18:13:36 +0700

    update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use('/api/v1/bookings', bookings);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`);
+  console.log(`Server running in ${process.env.HOST} on port ${PORT}`);
 });
 
 // จัดการ unhandled rejection
