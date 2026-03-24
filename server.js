d836494fac9abc71451dec3a6164696105036569
unknown <jamecharun2007@gmail.com>
2026-03-24 17:38:41 +0700

    add: cors

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ app.set('query parser', 'extended');
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    process.env.CLIENT_URL
+  ],
   credentials: true
 }));
 
